87b0d62eb32aff5061b0c27208a27fcfff9bdba0
Zhi <2308981443@qq.com>
2017-08-13 12:19:12 +0800

    Update wordgame_3.py
    添加了引入random模组和对于while的循环用法修改

--- wordGame/wordgame_3.py
+++ wordGame/wordgame_3.py
@@ -1,15 +1,23 @@
-print("while 的用法演示");
+import random
+secret = random.randint(1,10);
 
+print("while 的用法演示");
+print(secret);
 temp = input("请输入你猜测的数字:");
-guess = int(temp); 
-while guess!=8:
-    
-    if guess >8 :
-        print("猜的数字大了");
-        temp = input("请重新输入你猜测的数字:");
-        guess = int(temp); 
+guess = int(temp);
+counter = 0;
+while guess!=secret and counter<2:
+    print("您猜错了");
+    temp = input("请重新输入你猜测的数字:");
+    guess = int(temp);
+    counter = counter+1;
+    if guess == secret:
+        print("恭喜您猜对了");
     else:
-        print("猜的数字小了");
-        temp = input("请重新输入你猜测的数字:");
-        guess = int(temp); 
-print("恭喜您猜对了！");
+ 
+        if guess >secret:
+            print("猜的数字大了");
+        else:
+            print("猜的数字小了");
+
+print("游戏结束！");
